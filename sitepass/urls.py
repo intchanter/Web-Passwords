2177bdf80ab5e726e27f4990aa1983bd5de6c5eb
Intchanter <unlearned@gmail.com>
2010-03-11 08:45:15 -0700

    Add osCommerce support

--- sitepass/urls.py
+++ sitepass/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls.defaults import *
 from views import joomla
+from views import oscommerce
 
 urlpatterns = patterns('',
     ('^joomla/$', joomla),
+    ('^oscommerce/$', oscommerce),
     #('', ''),
 )
