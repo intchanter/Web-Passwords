016b60f6cf72bc7e767820ce66f8f7536a5d15a4
Intchanter <unlearned@gmail.com>
2010-04-28 20:57:00 -0600

    Clean missed refactor artifact

--- sitepass/frameworks/oscommerce.py
+++ sitepass/frameworks/oscommerce.py
@@ -12,15 +12,3 @@ def hash(form):
     password = form.cleaned_data['password']
     salt = randstring(alphanum, 2)
     return md5(salt + password).hexdigest() + ':' + salt
-
-
-def aoeuoscommerce(request):
-    hash = None
-    form = forms.OSCommerceForm()
-    if request.method == 'POST':
-        form = forms.OSCommerceForm(request.POST)
-        if form.is_valid():
-            password = string.strip(request.POST['password'])
-            salt = randstring(string.ascii_letters + string.digits, 2)
-            hash = md5(salt + password).hexdigest() + ':' + salt
-    return render_to_response('sitepass.html', {'title': 'osCommerce Password Generation Utility', 'hash': hash, 'form': form})
