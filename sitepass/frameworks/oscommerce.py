35c10292beffed363859597dc7fd4eae8f5d7b98
Intchanter <unlearned@gmail.com>
2010-05-17 10:48:15 -0600

    More accurate password generation for oscommerce

--- sitepass/frameworks/oscommerce.py
+++ sitepass/frameworks/oscommerce.py
@@ -7,7 +7,13 @@ help_text = '''<p>help_text</p>'''
 
 from sitepass.forms import OSCommerceForm as form_class
 
+# This algorithm appears to defeat the purpose of salt by guaranteeing
+# that the same database entry will always be generated for a given
+# password.  Assuming it is shown to work, prefer a random salt over
+# purity.
 def hash(form):
     password = form.cleaned_data['password']
-    salt = randstring(alphanum, 2)
+    temp = md5(password).hexdigest()
+    #salt = randstring(alphanum, 2)
+    salt = temp[0:2]
     return md5(salt + password).hexdigest() + ':' + salt
