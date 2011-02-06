c2582350dcdadd8f01490221a20e75147d9e59c2
Intchanter <unlearned@gmail.com>
2011-02-05 23:08:19 -0700

    New app for home page.

---
+++ home/views.py
@@ -0,0 +1,7 @@
+# Create your views here.
+from passwordsoup.sitepass.views import menu
+from articles.views import display_blog_page
+from django.shortcuts import render_to_response
+
+def home(request):
+    return render_to_response('home.html')
