0570dff1a2cff86b7fc34e83892b4252ececb72e
Intchanter <unlearned@gmail.com>
2011-02-05 23:38:56 -0700

    Added basic Concrete5 howto information.

--- sitepass/frameworks/concrete5.py
+++ sitepass/frameworks/concrete5.py
@@ -4,7 +4,14 @@ name = 'Concrete5'
 url = 'concrete5'
 title = 'Generate %s Password Hash' % name
 
-help_text = '''<p>help_text</p>'''
+help_text = '''
+<p>Enter your desired password in the Password field, then fill the Salt field
+using the information from the config/site.php file in your Concrete5
+installation.  Be sure to grab the whole salt value, without the enclosing
+quote marks.</p>
+<p>After you submit the form, you will be provided with the database entry,
+which you can set directly in the database.</p>
+'''
 
 from sitepass.forms import Concrete5Form as form_class
 
