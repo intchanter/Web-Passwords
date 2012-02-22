b8fa00ff0777b6de26805a09ff6a8642d67c8faf
Intchanter <unlearned@gmail.com>
2012-02-22 05:16:36 -0500

    Try to make the menu site-wide

--- home/views.py
+++ home/views.py
@@ -4,4 +4,4 @@ from articles.views import display_blog_page
 from django.shortcuts import render_to_response
 
 def home(request):
-    return render_to_response('home.html')
+    return render_to_response('home.html', {'menu': menu(request)})
